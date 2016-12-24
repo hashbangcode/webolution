4ce3f91ac13dbafa4642bec1485accf0b93d0169
Philip Norton <philipnorton42@gmail.com>
2016-12-24 23:32:03 +0000

    added tests for the Text type class

---
+++ Tests/Type/Text/TextTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use Hashbangcode\Wevolution\Type\Text\Text;
+
+/**
+ * Test class for Color
+ */
+class TextTest extends PHPUnit_Framework_TestCase
+{
+
+  public function testCreateText() {
+    $object = new Text('fhsdlakjfhalksjdfhl');
+    $this->assertInstanceOf('Hashbangcode\Wevolution\Type\Text\Text', $object);
+  }
+
+  public function testRenderText() {
+    $text = 'fhsdlakjfhalksjdfhl';
+    $object = new Text($text);
+    $this->assertEquals($text, $object->render());
+  }
+}
