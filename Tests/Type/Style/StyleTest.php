8bf5b262bd4c357ee5887c5a83256488ec205718
Philip Norton <philipnorton42@gmail.com>
2016-12-24 23:32:18 +0000

    added style class and tests

--- Tests/Type/Style/StyleTest.php
+++ Tests/Type/Style/StyleTest.php
@@ -12,4 +12,47 @@ class StyleTest extends PHPUnit_Framework_TestCase
     $object = new Style('.element');
     $this->assertInstanceOf('Hashbangcode\Wevolution\Type\Style\Style', $object);
   }
+
+  public function testSetAndGetAttributes() {
+    $object = new Style('.element');
+    $object->setAttributes(['background' => 'black']);
+    $attributes = $object->getAttributes();
+    $this->assertEquals('black', $attributes['background']);
+  }
+
+  public function testFullObjectCreation() {
+    $object = new Style('.element', ['background' => 'black']);
+    $attributes = $object->getAttributes();
+    $this->assertEquals('black', $attributes['background']);
+  }
+
+  public function testRenderSimpleStyle() {
+    $object = new Style('.element', ['background' => 'black']);
+    $renderedStyle = $object->render();
+    $this->assertEquals('.element{background:black;}', $renderedStyle);
+  }
+
+  public function testChangeSelector() {
+    $object = new Style('.element', ['background' => 'black']);
+    $object->setSelector('.div');
+    $this->assertEquals('.div', $object->getSelector());
+  }
+
+  public function testGetAttribute() {
+    $object = new Style('.element', ['background' => 'black']);
+    $this->assertEquals('black', $object->getAttrbute('background'));
+  }
+
+  public function testGetNonExistingAttribute() {
+    $object = new Style('.element', ['background' => 'black']);
+    $this->assertEquals(false, $object->getAttrbute('color'));
+  }
+
+  public function testRenderMoreComplexStyle() {
+    $object = new Style('.element', ['background' => 'black']);
+    $object->setAttrbute('color', 'red');
+    $object->setAttrbute('padding', '0px');
+    $renderedStyle = $object->render();
+    $this->assertEquals('.element{background:black;color:red;padding:0px;}', $renderedStyle);
+  }
 }
--- src/Type/Style/Style.php
+++ src/Type/Style/Style.php
@@ -2,13 +2,122 @@
 
 namespace Hashbangcode\Wevolution\Type\Style;
 
-class Style {
+class Style
+{
 
-  public function __construct() {
+  /**
+   * The style selector.
+   *
+   * @var null|string
+   */
+  protected $selector = '';
 
+  /**
+   * Get the selector.
+   *
+   * @return null|string
+   */
+  public function getSelector()
+  {
+    return $this->selector;
   }
 
-  public function render() {
+  /**
+   * Get the selector.
+   *
+   * @param null|string $selector
+   */
+  public function setSelector($selector)
+  {
+    $this->selector = $selector;
+  }
 
+  /**
+   * The style attributes.
+   *
+   * @var array
+   */
+  protected $attributes = [];
+
+  /**
+   * Style constructor.
+   *
+   * @param null $selector
+   * @param null $attributes
+   */
+  public function __construct($selector = NULL, $attributes = NULL)
+  {
+    if (!is_null($selector)) {
+      $this->selector = $selector;
+    }
+
+    if (!is_null($attributes) && is_array($attributes)) {
+      $this->attributes = $attributes;
+    }
+  }
+
+  /**
+   * Get all attributes.
+   *
+   * @return array
+   */
+  public function getAttributes()
+  {
+    return $this->attributes;
+  }
+
+  /**
+   * Set all attributes.
+   *
+   * @param array $attributes
+   */
+  public function setAttributes($attributes)
+  {
+    $this->attributes = $attributes;
+  }
+
+  /**
+   * Set an attrobite.
+   *
+   * @param $name The name of the attribute.
+   * @param $value The value to set the attribute to.
+   */
+  public function setAttrbute($name, $value)
+  {
+    $this->attributes[$name] = $value;
+  }
+
+  /**
+   * Get a single attribute value.
+   *
+   * @param $name The attribute name.
+   *
+   * @return bool|mixed The attribute value, or false if the attribute is not present.
+   */
+  public function getAttrbute($name)
+  {
+    if (isset($this->attributes[$name])) {
+      return $this->attributes[$name];
+    }
+
+    return false;
+  }
+
+  /**
+   * Render the style.
+   */
+  public function render()
+  {
+    $output = '';
+
+    $output .= $this->getSelector() . '{';
+
+    foreach ($this->getAttributes() as $attribute => $value) {
+      $output .= $attribute . ':' . $value . ';';
+    }
+
+    $output .= '}';
+
+    return $output;
   }
 }
