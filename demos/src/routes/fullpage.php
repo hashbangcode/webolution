a3dcf01a35f9138d271a5d1a4e54ab0fb06897f4
Philip Norton <philipnorton42@gmail.com>
2016-12-31 22:25:01 +0000

    updated the full page html test

--- demos/src/routes/fullpage.php
+++ demos/src/routes/fullpage.php
@@ -3,17 +3,30 @@
 use Hashbangcode\Wevolution\Evolution\Evolution;
 use Hashbangcode\Wevolution\Evolution\Population\ElementPopulation;
 use Hashbangcode\Wevolution\Type\Element\Element;
+use Hashbangcode\Wevolution\Type\Style\Style;
 use Hashbangcode\Wevolution\Evolution\EvolutionStorage;
 use Hashbangcode\Wevolution\Evolution\Individual\ElementIndividual;
+use Hashbangcode\Wevolution\Evolution\Individual\StyleIndividual;
 
 $app->get('/fullpage_test', function ($request, $response, $args) {
-  $styles = 'div {width:10px;height:10px;display:inline-block;padding:0px;margin:0px;}';
+  $styles = 'div {width:10px;height:10px;display:inline-block;padding:0px;margin:0px;}.elementframe{width:500px;height:500px;}';
 
   $title = 'Full Page Test';
 
   $population = new ElementPopulation();
 
   $html = new Element('html');
+  $head = new Element('head');
+
+  $style = new Element('style');
+  $style_object = new StyleIndividual('.text');
+  $style_element = new Element($style_object);
+  $style->addChild($style_element);
+
+  $head->addChild($style);
+
+  $html->addChild($head);
+
   $body = new Element('body');
   $div = new Element('div');
   $p1 = new Element('p');
@@ -32,8 +45,14 @@ $app->get('/fullpage_test', function ($request, $response, $args) {
 
   $output = '';
 
+  $output .= '<h2>HTML</h2>';
+
   $population->setDefaultRenderType('htmltextarea');
   $output .= $population->render();
+
+  $output .= '<br>';
+
+  $output .= '<h2>iFrame</h2>';
 
   $population->setDefaultRenderType('html');
   $output .= $population->render();
