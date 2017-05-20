08764682ed31fa65cc1757a08da49aaa0df69b8c
Philip Norton <philipnorton42@gmail.com>
2017-05-20 17:39:47 +0100

    added more tests for the Page class

--- tests/Type/Page/PageTest.php
+++ tests/Type/Page/PageTest.php
@@ -110,4 +110,36 @@ class PageTest extends \PHPUnit_Framework_TestCase
         $element_selectors = $element->getAllSelectors();
         $this->assertEquals($page_selectors, $element_selectors);
     }
+
+    public function testGetAllTypes()
+    {
+        $object = new Page();
+
+        $element = new Element('div');
+        $inner_element = new Element('p');
+        $inner_element->setAttribute('class', 'paragraph');
+        $element->addChild($inner_element);
+        $object->setBody($element);
+
+        // The page and body elements should both produce the same selectors list.
+        $page_selectors = $object->getBodyElementTypes();
+        $element_selectors = $element->getAllTypes();
+        $this->assertEquals($page_selectors, $element_selectors);
+    }
+
+    public function testGetAllClasses()
+    {
+        $object = new Page();
+
+        $element = new Element('div');
+        $inner_element = new Element('p');
+        $inner_element->setAttribute('class', 'paragraph');
+        $element->addChild($inner_element);
+        $object->setBody($element);
+
+        // The page and body elements should both produce the same selectors list.
+        $page_selectors = $object->getBodyClasses();
+        $element_selectors = $element->getAllClasses();
+        $this->assertEquals($page_selectors, $element_selectors);
+    }
 }
