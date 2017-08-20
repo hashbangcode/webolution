2af93e49fe64dad9cd1cd4844cbef83a2549c621
Philip Norton <philipnorton42@gmail.com>
2017-08-20 23:44:15 +0100

    Added crossover to the text population class.

--- src/Evolution/Population/TextPopulation.php
+++ src/Evolution/Population/TextPopulation.php
@@ -63,6 +63,51 @@ class TextPopulation extends Population
      */
     public function crossover()
     {
-        // @todo implement this.
+        // Get two individuals from the population.
+        $individuals = $this->getRandomIndividuals(2);
+
+        // Make sure we have Individuals to use.
+        if ($individuals == false) {
+            // Add a clone of a individual individual.
+            $randomIndividual = $this->getRandomIndividual();
+            $this->addIndividual(clone $randomIndividual);
+
+            // Don't do anything else.
+            return;
+        }
+
+        $text1 = str_split($individuals[0]->getObject()->getText());
+        $text2 = str_split($individuals[1]->getObject()->getText());
+
+        $newText = '';
+
+        $text1Count = count($text1);
+        $text2Count = count($text2);
+
+
+        if ($text1Count >= $text2Count) {
+            foreach ($text1 as $id => $letter) {
+                if ($id % 2 || !isset($text2[$id])) {
+                    $newText .= $text1[$id];
+                } elseif (isset($text2[$id])) {
+                    $newText .= $text2[$id];
+                }
+            }
+        } elseif ($text1Count < $text2Count) {
+            foreach ($text2 as $id => $letter) {
+                if ($id % 2 || !isset($text1[$id])) {
+                    $newText .= $text2[$id];
+                } elseif (isset($text1[$id])) {
+                    $newText .= $text1[$id];
+                }
+            }
+        }
+
+
+        // Create a new individual.
+        $individualNew = TextIndividual::generateFromString($newText);
+
+        // Add the individual to the population.
+        $this->addIndividual($individualNew);
     }
 }
