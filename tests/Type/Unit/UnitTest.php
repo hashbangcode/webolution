814bf17b3b30c4a98b27917785610cdec56c7d75
Phil Norton <phil.norton@weareaccess.co.uk>
2018-06-19 08:44:11 +0100

    Added tests for getting and setting numbers and units in the unit class.

--- tests/Type/Unit/UnitTest.php
+++ tests/Type/Unit/UnitTest.php
@@ -10,10 +10,26 @@ use Hashbangcode\Wevolution\Type\Unit\Unit;
 class UnitTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function testCreateText()
+    public function testCreateUnit()
     {
         $object = new Unit(1, 'px');
         $this->assertInstanceOf('Hashbangcode\Wevolution\Type\Unit\Unit', $object);
+    }
+
+    public function testSetAndGetUnit()
+    {
+        $object = new Unit(1, 'px');
+        $this->assertEquals('px', $object->getUnit());
+        $object->setUnit('em');
+        $this->assertEquals('em', $object->getUnit());
+    }
+
+    public function testSetAndGetNumber()
+    {
+        $object = new Unit(1, 'px');
+        $this->assertEquals(1, $object->getNumber());
+        $object->setNumber(2);
+        $this->assertEquals(2, $object->getNumber());
     }
 
     public function testInvalidUnit()
