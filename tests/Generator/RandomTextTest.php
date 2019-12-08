902a4f5556c1288ceb3d5d32f0c40ece2432631e
Philip Norton <philipnorton42@gmail.com>
2019-12-08 20:28:35 +0000

    Added faker library.
    Updated the RandomText trait to include functions that generate a few words or a few sentences.
    Updated the Element class to allow for a few words to be added to title elements and sentences to be added to text elements.

--- tests/Generator/RandomTextTest.php
+++ tests/Generator/RandomTextTest.php
@@ -25,4 +25,20 @@ class RandomTextTest extends TestCase
         $this->assertTrue(is_string($letter));
         $this->assertEquals(10, strlen($letter));
     }
+
+    public function testGenerateFakeTitle()
+    {
+        $title = $this->generateFakeTitle();
+        $this->assertTrue(is_string($title));
+        $this->assertGreaterThan(1, strlen($title));
+        $this->assertLessThan(15, strlen($title));
+    }
+
+    public function testGenerateFakeText()
+    {
+        $text = $this->generateFakeText();
+        $this->assertTrue(is_string($text));
+        $this->assertGreaterThan(1, strlen($text));
+        $this->assertLessThan(300, strlen($text));
+    }
 }
